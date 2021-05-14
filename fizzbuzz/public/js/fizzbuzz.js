b30ff0451e8d170fd080d59ad7b43377b93509f8
Robson Souza Lourenço <robinhosl2008@gmail.com>
2021-05-14 17:38:37 -0300

    Adicionando o novo link para reversão de strings.

--- fizzbuzz/public/js/fizzbuzz.js
+++ fizzbuzz/public/js/fizzbuzz.js
@@ -10,7 +10,7 @@
 
 function init(){
     var valor = document.getElementById("valor"),
-        resultado = document.getElementById("resultado");
+        resultado = document.getElementById("oResultado");
 
     document.getElementById("limpar").onclick = function() {
         valor.value = "";
@@ -18,10 +18,10 @@ function init(){
     };
 
     document.getElementById("voltar").onclick = function() {
-        window.location.href = "../index.html";
+        window.location.href = "../../../index.html";
     }
 
-    document.getElementById("validar").onclick = function() {
+    document.getElementById("executar").onclick = function() {
         let obj = {
             "val": valor.value, 
             "elemRes": resultado
@@ -34,23 +34,15 @@ function init(){
 function mostrarResultado(obj) {
     
     if (isNaN(obj.val)) {
-        obj.elemRes.value = "Não é um número.";
+        obj.elemRes.innerText = "Não é um número.";
     } else if (obj.val % 3 == 0 && obj.val % 5 == 0) {
-        obj.elemRes.value = "FizzBuzz";
+        obj.elemRes.innerText = "FizzBuzz";
     } else if (obj.val % 3 == 0) {
-        obj.elemRes.value = "Fizz";
+        obj.elemRes.innerText = "Fizz";
     } else if (obj.val % 5 == 0) {
-        obj.elemRes.value = "Buzz";
+        obj.elemRes.innerText = "Buzz";
     } else {
-        obj.elemRes.value = obj.val;
+        obj.elemRes.innerText = obj.val;
     }
     
 }
-
-/*
-trabalho com a linguagem php
-mas também javascript. Mais no front-end.
-Trabalhei no ministério da saúde e lá 
-também trabalhava com php. E ainda estou com
-php.
-*/
